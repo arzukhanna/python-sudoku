7121c91793379e61fe7604476503cd685fb4c0ed
Arzu Khanna <arzu.khanna@marlo.com.au>
2021-07-01 18:00:03 +1000

    updated library and main programs

--- lib/sudoku.py
+++ lib/sudoku.py
@@ -1,14 +1,52 @@
 import numpy
 
 
+def read_grid(file):
+    """
+    :param file: file containing sudoku puzzle to be solved
+    :return: list of lists representing sudoku puzzle
+    """
+    grid = []
+    file = open(file, 'r', encoding='utf-8')
+    for line in file:
+        grid.append([int(n) for n in line.split()])
+
+
+def is_grid_valid(grid):
+    """
+    :param grid: list of lists representing sudoku puzzle
+    :return: TRUE if grid is valid, FALSE otherwise
+    """
+    # 1. Check there are 9 rows
+    if len(grid) != 9:
+        return False
+
+    # 2. Check all rows have 9 numbers
+    for row in grid:
+        if len(grid[row]) != 9:
+            return False
+
+    # 3. Check that grid contains only integers in range [0...9]
+    for row in grid:
+        for i in row:
+            try:
+                if not (0 <= i <= 9):
+                    return False
+            # if i is not an integer then a TypeError will arise, return False
+            except TypeError:
+                return False
+    return True
+
+
 def possible(grid, y, x, n):
     """
+    function: determines whether inputting n in a particular cell (grid[y][x]) is possible according to sudoku rules
+    :param grid: current state of sudoku puzzle
     :param y: row
     :param x: column
     :param n: number in cell
-    :return: TRUE if possible for nn to go in cell, FALSE otherwise
+    :return: TRUE if possible for n to go in cell, FALSE otherwise
     """
-    # global grid
     for i in range(9):
         if grid[y][i] == n:
             return False
@@ -25,7 +63,10 @@ def possible(grid, y, x, n):
 
 
 def solve_sudoku(grid):
-    #global grid
+    """
+    function: iterates through each cell in the puzzle to determine which cells need to be solved
+    :param grid: current state of sudoku puzzle
+    """
     for y in range(9):
         for x in range(9):
             if grid[y][x] == 0:
--- solve-sudoku.py
+++ solve-sudoku.py
@@ -1,42 +1,28 @@
-from lib.sudoku import possible, solve_sudoku
+from lib.sudoku import solve_sudoku, read_grid, is_grid_valid
 import argparse
 import sys
-import os.path
 
 if __name__ == "__main__":
 
-    __version__ = '3.8.3'
+    __version__ = '0.1.0'
 
-    parser = argparse.ArgumentParser(description='Solve 9x9 sudoku puzzle')
+    parser = argparse.ArgumentParser(description='Solve 9x9 sudoku puzzle',
+                                     epilog='© 2021 Arzu Khanna mailto:arzukhanna@marlo.com')
 
-    parser.add_argument('infile',
-                        nargs='?',
-                        type=argparse.FileType('r'),
-                        default=sys.stdin,
+    parser.add_argument('-p', '--puzzle',
+                        type=argparse.FileType('r', encoding='utf-8'),
                         help='file that contains sudoku puzzle as grid')
 
     parser.add_argument('--version',
                         action='version',
-                        version='<the version>')
+                        version=__version__)
 
     args = parser.parse_args()
 
-    grid = []
-    for f in args.file:
-        a_list = parser.convert_arg_line_to_args(f)
-        map_object = map(int, a_list)
-        list_ints = list(map_object)
-        grid.append(list_ints)
-    solution = solve_sudoku(grid)
-    print(solution)
+    grid = read_grid(args.puzzle)
 
-    # grid = []
-    # file = open('medium.txt', 'r')
-    # for line in file:
-    #     line = line.strip('\n')
-    #     a_list = line.split()
-    #     map_object = map(int, a_list)
-    #     list_ints = list(map_object)
-    #     grid.append(list_ints)
-    # solution = solve_sudoku(grid)
-    # print(solution)
+    if is_grid_valid(grid):
+        print(solve_sudoku(grid))
+    else:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
